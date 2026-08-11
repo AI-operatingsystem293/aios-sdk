46724d97bb28f03feb1ef061ca9fa5303269c0c6
AI-operatingsystem293 <prosperboateng298@gmail.com>
2026-08-11 23:30:50 +0000

    Fix Rust unsafe no_mangle plugin export

--- src/export.rs
+++ src/export.rs
@@ -1,10 +1,8 @@
-use crate::agent::Agent;
-
 #[macro_export]
 macro_rules! export_agent_plugin {
     ($agent:ty) => {
-        #[no_mangle]
-        pub extern "C" fn create_plugin() -> *mut dyn Agent {
+        #[unsafe(no_mangle)]
+        pub extern "C" fn create_plugin() -> *mut dyn $crate::agent::Agent {
             let agent = <$agent>::new();
             Box::into_raw(Box::new(agent))
         }
